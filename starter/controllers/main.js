86ebfd07baccef15a1fc13678fb0d70ce4954ace
Krupalsinh Chavda <krupalsinhchavda36143@gmail.com>
2022-11-21 16:29:42 +0530

    JWT Basics commit

---
+++ starter/controllers/main.js
@@ -0,0 +1,26 @@
+
+const jwt = require('jsonwebtoken')
+const {BadRequestError} = require('../errors')
+const login = async(req, res) =>{
+    const {username,password} = req.body
+    if(!username || !password){
+        throw new BadRequestError('please provide email and password')
+    }
+
+    const id = new Date().getDate()
+    // try to keep payload small, better eperience for user
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+    
+    res.status(200).json({msg:'user created',token})
+}
+
+const dashboard = async(req, res) =>{
+   
+    const luckyNumber = Math.floor( Math.random() * 100)
+    res.status(200).json({msg:`hello, ${req.user.username}`,secret:`here is your authorized data your lucknumber is ${luckyNumber}`})
+
+    }
+
+module.exports = {
+    login,dashboard
+}
